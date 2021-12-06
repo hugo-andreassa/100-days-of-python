416151ee23b17ebcad13aa351b7fc6d59cef1cd1
Hugo Andreassa <hugo.andreassa@gmail.com>
2021-12-06 20:27:12 -0300

    Python projects

--- day-38-workout-google-sheets/main.py
+++ day-38-workout-google-sheets/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import requests
+from datetime import date, datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+API_NUTRI_URL = "https://trackapi.nutritionix.com/"
+API_SHEET_URL = "https://api.sheety.co/dfba959c9fc7d64f75d73a39ef8ac16e/myWorkouts/workouts"
+
+APPLICATION_ID = "2c4ce77c"
+API_NUTRI_KEY = "9b56c1d7164bb6f65f5ce28ffa9fb1b1"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def parse_exercises_to_json_via_nutri_api(exercises: str) -> [str]:
+    nutri_headers = {
+        "x-app-id": APPLICATION_ID,
+        "x-app-key": API_NUTRI_KEY,
+        "x-remote-user-id": "0"
+    }
+    nutri_params = {
+        "query": exercises
+    }
+
+    response = requests.post(f"{API_NUTRI_URL}/v2/natural/exercise", json=nutri_params, headers=nutri_headers)
+    response.raise_for_status()
+    return response.json()["exercises"]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def save_exercise_to_google_sheet(exercise: str, duration: int, calories: int):
+    today = datetime.today()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sheet_params = {
+        "workout": {
+            "date": today.strftime("%d/%m/%Y"),
+            "time": today.strftime("%H:%M:%S"),
+            "exercise": exercise.title(),
+            "duration": duration,
+            "calories": calories
+        }
+    }
+    response = requests.post(API_SHEET_URL, json=sheet_params)
+    response.raise_for_status()
+    # print(response.text)
+
+
+exercises = input("Tell me which exercise do you did: ")
+
+nutri_data = parse_exercises_to_json_via_nutri_api(exercises)
+for item in nutri_data:
+    save_exercise_to_google_sheet(item["user_input"], item["duration_min"], item["nf_calories"])
